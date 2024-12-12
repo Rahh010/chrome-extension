e48b32bb6f1d6e97feaab867c8a3556665c635af
Aruthraa <aruthrasenthilkumar108@gmail.com>
2024-12-12 14:41:27 +0530

    frontend

--- Frontend/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Sidebar from './components/Sidebar';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <Sidebar />
-    </React.StrictMode>
-);
